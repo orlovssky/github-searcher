f393b64644529fbd85ce0ceca9f319667f3d74f8
Sanzhar Alimkhanov <orlovssky@protonmail.com>
2023-12-30 12:06:47 +0400

    added eslint rules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react(), tsconfigPaths() ],
+  css: {
+    postcss: {
+      plugins: [ autoprefixer() ], 
+    }, 
+  }, 
 })
+
