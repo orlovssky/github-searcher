edc7ae77ed1544d67d65de082f5187bad546631e
Sanzhar Alimkhanov <orlovssky@protonmail.com>
2024-01-04 17:28:28 +0400

    removed encoding

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
-import { decode } from 'js-base64'
 
 const client = new GraphQLClient('https://api.github.com/graphql', {
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `bearer ${decode(import.meta.env.VITE_GITHUB_TOKEN)}`
+    Authorization: `bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
   }
 })
 
